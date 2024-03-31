bc7c91f68bcf06add6072020f55275bcc7765d10
seiichi0404 <yamayamaseisei1834@gmail.com>
2024-03-31 13:48:08 +0900

    ヘッダー修正

--- src/Views/layout/header.php
+++ src/Views/layout/header.php
@@ -9,24 +9,24 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
     <link rel="stylesheet" href="../../public/css/style.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
-    <title>Online Image Hosting Service</title>
+    <title>Imageboard Webappe</title>
 </head>
 <body>
 
 <!-- Full-width Navbar Start -->
 <nav class="navbar navbar-expand-lg bg-dark border-bottom border-dark" data-bs-theme="dark">
     <div class="container-fluid">
-        <a class="navbar-brand" href="/top">Online-Image-Hosting-Service</a>
+        <a class="navbar-brand" href="/posts/library">Imageboard Webappe</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarColor01">
             <ul class="navbar-nav ms-auto">
                 <li class="nav-item">
-                    <a class="nav-link" href="/top">Home</a>
+                    <a class="nav-link" href="/posts/library">Home</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link" href="/imageLibrary">Image Library</a>
+                    <a class="nav-link" href="#">Profile</a>
                 </li>
             </ul>
         </div>
